de4d9e37ea1be1f7a3c4dd3591a26fe14fe5301b
Gin <poke5464@gmail.com>
2014-09-28 16:47:54 +0800

    remove 3rd-party's lib

--- wechat/common.py
+++ wechat/common.py
@@ -1,19 +1,12 @@
 import logging
 
-API_KEY = ''
-SECRET_KEY = ''
 
 def get_logger(name, level='debug'):
     logger = logging.getLogger(name)
     level = eval('logging.' + level.upper())
     logger.setLevel(level)
     if not logger.handlers:
-        try:
-            from bae_log import handlers
-            handler = handlers.BaeLogHandler(ak=API_KEY, sk=SECRET_KEY, bufcount=1)
-        except Exception, e:
-            handler = logging.StreamHandler()
-
+        handler = logging.StreamHandler()
         handler.setFormatter(logging.Formatter("|%(levelname)s - %(asctime)s - %(name)s - %(message)s"))
         logger.addHandler(handler)
     return logger
@@ -30,11 +23,5 @@ class Cache(object):
         self.__tmp[key] = value
 
 def get_cache():
-    try:
-        from bae_memcache import BaeMemcache
-        cache_id = ""
-        cache_addr = ""
-        cache = BaeMemcache(cache_id, cache_addr, API_KEY, SECRET_KEY)
-    except Exception, e:
-        cache = Cache()
+    cache = Cache()
     return cache
